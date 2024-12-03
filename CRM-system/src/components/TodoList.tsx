527e9f45f15c626a573f73ed57ea3a22ff52e515
Ilya Kravchenko <iliakravchenko09@gmail.com>
2024-12-03 14:47:56 +0500

    added all logic

--- CRM-system/src/components/TodoList.tsx
+++ CRM-system/src/components/TodoList.tsx
@@ -4,17 +4,17 @@ import { Todo } from '../types/todo';
 
 interface TodoListProps {
   tasks: Todo[];
-  filter: string;
+  filter: "all" | "completed" | "inWork";
   toggleTask: (id: number) => void;
   updateTaskText: (id: number, newText: string) => void;
   deleteTask: (id: number) => void;
 }
 
 const TodoList: React.FC<TodoListProps> = ({ tasks, filter, toggleTask, updateTaskText, deleteTask }) => {
-  const filterTasks = (tasks: Todo[], filter: string) => {
-    if (filter === "Всё") return tasks;
-    if (filter === "В работе") return tasks.filter(task => !task.isDone);
-    if (filter === "Сделано") return tasks.filter(task => task.isDone);
+  const filterTasks = (tasks: Todo[], filter: "all" | "completed" | "inWork") => {
+    if (filter === "all") return tasks;
+    if (filter === "inWork") return tasks.filter(task => !task.isDone);
+    if (filter === "completed") return tasks.filter(task => task.isDone);
     return tasks;
   };
 
